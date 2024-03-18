9aa6d3bc85acaa58c37856f54853bae3dd55ff9e
Himanshu Singh Bhandari <himanshubhandari4675@gmail.com>
2024-03-18 23:24:51 +0530

    adding Button Compnent

--- components/Button/button.js
+++ components/Button/button.js
@@ -8,12 +8,33 @@ export default function Button({ children , onClick}){
       onClick={onClick}
       whileTap={{scale: 0.9, transition: {duration: 0.2 }}}>
       <motion.div
+      initial={{scale : 0}}
+      whileInView={{
+        scale: 1,
+        width:[0, 60, 60, 180],
+        height: [0, 60, 60, 60],
+        borderRadius: [0, 60 , 60 , 30],
+        transition: { delay: 1, duration: 1},
+      }}
+      className="absolute z-10 bg-gray-200 flex flex-row justify-evenly items-center drop-shadow-lg cursor-pointer"
       >
-
+        <motion.p
+        initial={{opacity: 0}}
+        whileInView={{opacity:1 , transition: {delay: 2}}}
+        className="font-thin text-lg">
+          {children}
+        </motion.p>
       </motion.div>
-
+      <motion.div
+      initial={{opacity: 0 , scale: 0, zIndex: 1}}
+      whileInView={{
+        opacity: [0,1,0],
+        scale: [0,1.1,0],
+        transition: {delay: 1 , duration: 1},
+      }}
+      className="bg-blue-500 h-20 w-20 rounded-full">
       </motion.div>
-
+      </motion.div>
     </div>
   );
 }
