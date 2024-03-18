e75efaeb82d7347bd9c40d8cf15d2bff75abdacf
Himanshu Singh Bhandari <himanshubhandari4675@gmail.com>
2024-03-18 22:13:59 +0530

    working over Button component

--- components/Button/button.js
+++ components/Button/button.js
@@ -1,1 +1,19 @@
- 
+import {motion} from "framer-motion";
+
+export default function Button({ children , onClick}){
+  return (
+    <div className="scale-75 sm:scale-100">
+      <motion.div
+      className="absolute w-full bottom-[-15vh] flex flex-col justify-center items-center"
+      onClick={onClick}
+      whileTap={{scale: 0.9, transition: {duration: 0.2 }}}>
+      <motion.div
+      >
+
+      </motion.div>
+
+      </motion.div>
+
+    </div>
+  );
+}
