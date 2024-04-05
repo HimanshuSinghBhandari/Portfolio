f3cd70a25b67a397f7da8424484e6d42c5bacfe3
Himanshu Singh Bhandari <himanshubhandari4675@gmail.com>
2024-04-05 23:26:07 +0530

    ContactIconList.js

--- components/ContactPage/ContactIcon.js
+++ components/ContactPage/ContactIcon.js
@@ -1,1 +1,19 @@
- 
+ import {motion} from "framer-motion";
+
+ export default function ContactIcon({title, url , children})
+ {
+    return (
+        <motion.li 
+        whileHover={{ scale: 1.2}}
+        whileTap={{ scale: 0.9, transition: {duration: 0.2 }}}>
+            <a 
+            title={title}
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block scale-75 lg:scale-100">
+            {children}
+            </a> 
+        </motion.li>
+    )
+ }
--- components/ContactPage/ContactIconList.js
+++ components/ContactPage/ContactIconList.js
@@ -1,6 +1,8 @@
 import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
 import { FaInstagram } from "@react-icons/all-files/fa/FaInstagram";
-import { FaLaptopCode } from "@react-icons/all-files/fa/FaLaptopCode";
+import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter";
+import {FaLinkedin} from "@react-icons/all-files/fa/FaLinkedin"
+//import { FaLaptopCode } from "@react-icons/all-files/fa/FaLaptopCode";
 import { MdEmail } from "@react-icons/all-files/md/MdEmail";
 import {motion} from "framer-motion";
 import ContactIcon from "./ContactIcon";
@@ -8,8 +10,32 @@ import ContactIcon from "./ContactIcon";
 export default function ContactIconList()
 {
     return (
-        <motion.ul>
-            
+        <motion.ul
+        className="flex flex-row justify-center lg:mt-5 lg:gap-20 items-center"
+        initial={{opacity: 0, y: 50}}
+        whileInView={{
+            opacity: 1,
+            y: 0,
+            transition: {delay: 0.5},
+        }}>
+            <ContactIcon
+            title="Linkdin"
+            url="https://www.linkedin.com/in/himanshu-singh-5226a628a"
+            >
+                <FaLinkedin size="70" />
+            </ContactIcon>
+            <ContactIcon title="GitHub" url="https://github.com/HimanshuSinghBhandari">
+                <FaGithub size="70"/>
+            </ContactIcon>
+            <ContactIcon title="Instagram" url="https://www.instagram.com/imhimanshubhandari">
+                <FaInstagram size="75" />
+            </ContactIcon>
+            <ContactIcon title="gmail" url="himanshubhandari4675@gmail.com">
+                <MdEmail size="75" />
+            </ContactIcon>
+            <ContactIcon title="twitter" url="https://twitter.com/himanshu_14rt">
+                <FaTwitter size="75" />
+            </ContactIcon>
         </motion.ul>
     )
 }
