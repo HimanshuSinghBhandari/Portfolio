d91631c31ca4b04b5530225d0b48f91518d7591e
Himanshu Singh Bhandari <himanshubhandari4675@gmail.com>
2024-04-05 23:47:35 +0530

    adding some features over Contact page

--- components/ContactPage/contact.js
+++ components/ContactPage/contact.js
@@ -2,4 +2,23 @@
  import {motion} from "framer-motion";
  import ContactPic from "../../public/assets/profile/mini.png.jpeg";
  import HeaderTitle from "../util/HeaderTitle";
- 
+ import ContactIconList from "./ContactIconList";
+
+ export default function Contact() {
+    return (
+        <section>
+            <HeaderTitle title=" CONTACT "/>
+            <div>
+                <Image>
+
+                </Image>
+            </div>
+            <div>
+                <motion.div>
+                    <h3>CONTACT ME</h3>
+                </motion.div>
+                <ContactIconList />
+            </div>
+        </section>
+    )
+ }
